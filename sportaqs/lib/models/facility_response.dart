426cb6538f5b635a4e878c61bcaec779fa3de7ec
santogarrido <santocildesgarridoraul@gmail.com>
2026-02-02 22:04:40 +0100

    Court and facility models and get facilities endpoint

--- sportaqs/lib/models/facility_response.dart
+++ sportaqs/lib/models/facility_response.dart
@@ -1,1 +1,21 @@
-f
+import 'package:sportaqs/models/facility.dart';
+
+class FacilitiesResponse {
+  final bool success;
+  final List<Facility> data;
+  final String message;
+
+  FacilitiesResponse({
+    required this.success,
+    required this.data,
+    required this.message,
+  });
+
+  factory FacilitiesResponse.fromJson(Map<String, dynamic> json) => FacilitiesResponse(
+    success: json["success"],
+    data: (json["data"] as List)
+        .map((facilityJson) => Facility.fromFacilitiesJson(facilityJson))
+        .toList(),
+    message: json["message"],
+  );
+}
