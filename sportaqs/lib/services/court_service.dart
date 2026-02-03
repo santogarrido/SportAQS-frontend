83b61a69d64eaaadbf8da7798aeb694d0fbf9082
santogarrido <santocildesgarridoraul@gmail.com>
2026-02-03 12:12:21 +0100

    Court and facility service

--- sportaqs/lib/services/court_service.dart
+++ sportaqs/lib/services/court_service.dart
@@ -7,11 +7,10 @@ class CourtService {
   static const String _baseUrl = 'http://localhost:8090';
 
   Future<CourtsResponse> getCourts(int id, String token) async {
-    Uri url = Uri.parse('$_baseUrl/courts/facility/');
+    Uri url = Uri.parse('$_baseUrl/courts/facility/$id');
 
-    final response = await http.post(
+    final response = await http.get(
       url,
-      body: json.encode({'id' : id}),
       headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'}
     );
 
@@ -19,6 +18,87 @@ class CourtService {
 
     return courtResponse;
 
+  }
+
+  Future<CourtsResponse> getCourtById(int id, String token) async {
+    Uri url = Uri.parse('$_baseUrl/courts/$id');
+
+    final response = await http.get(
+      url,
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'}      
+    );
+
+    final courtResponse = CourtsResponse.fromJson(json.decode(response.body));
+
+    return courtResponse;
+
+  }
+
+  Future<CourtsResponse> addCourt(String name, String category, int bookingDuration, int facilityId, String token) async {
+    Uri url = Uri.parse('$_baseUrl/courts');
+
+    final response = await http.post(
+      url,
+      body: ({'name' : name, 'category' : category, 'bookingDuration' : bookingDuration, 'facilityId' : facilityId}),
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'}
+    );
+
+    final courtResponse = CourtsResponse.fromJson(json.decode(response.body));
+
+    return courtResponse;
+  }
+
+  Future<CourtsResponse> updateCourt(int id, String name, String category, int bookingDuration, int facilityId, String token) async {
+    Uri url = Uri.parse('$_baseUrl/courts/$id');
+
+    final response = await http.put(
+      url,
+      body: ({'name' : name, 'category' : category, 'bookingDuration' : bookingDuration, 'facilityId' : facilityId}),
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'}
+    );
+
+    final courtResponse = CourtsResponse.fromJson(json.decode(response.body));
+
+    return courtResponse;
+  }
+
+  Future<CourtsResponse> deleteCourt(int id, String token) async {
+    Uri url = Uri.parse('$_baseUrl/courts/$id');
+
+    final response = await http.delete(
+      url,
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'}
+    );
+
+    final courtResponse = CourtsResponse.fromJson(json.decode(response.body));
+
+    return courtResponse;
+  }
+
+  Future<CourtsResponse> activateCourt(int id, String token) async {
+    Uri url = Uri.parse('$_baseUrl/courts/$id/activate');
+
+    final response = await http.put(
+      url,
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'}
+    );
+
+    final courtResponse = CourtsResponse.fromJson(json.decode(response.body));
+
+    return courtResponse;
+  }
+
+  Future<CourtsResponse> deactivateCourt(int id, String token) async {
+    Uri url = Uri.parse('$_baseUrl/courts/$id/deactivate');
+
+    final response = await http.put(
+      url,
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'}
+    );
+
+    final courtResponse = CourtsResponse.fromJson(json.decode(response.body));
+
+    return courtResponse;
   }
 
 }
---
+++ sportaqs/lib/services/facility_service.dart
@@ -0,0 +1,109 @@
+import "dart:convert";
+
+import "package:http/http.dart" as http;
+import "package:sportaqs/models/facility_response.dart";
+
+class FacilityService {
+
+  static const String _baseUrl = 'http://localhost:8090'; 
+
+  Future<FacilitiesResponse> getFacilities(String token) async {
+
+    Uri url = Uri.parse('$_baseUrl/getAll');
+
+    final response = await http.get(
+      url,
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'}
+    );
+
+    final facilityResponse = FacilitiesResponse.fromJson(json.decode(response.body));
+
+    return facilityResponse;
+  }
+
+  Future<FacilitiesResponse> getFacilityById(int id, String token) async {
+    Uri url = Uri.parse('$_baseUrl/getFacility/$id');
+
+    final response = await http.get(
+      url,
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'}
+    );
+
+    final facilityResponse = FacilitiesResponse.fromJson(json.decode(response.body));
+
+    return facilityResponse; 
+
+  }
+
+  Future<FacilitiesResponse> addFacility(String name, String openTime, String closeTime, String location, String token) async {
+    Uri url = Uri.parse('$_baseUrl/addFacility');
+
+    final response = await http.post(
+      url,
+      body: ({'name' : name, 'openTime' : openTime, 'closeTime' : closeTime, 'location' : location}),
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'}
+    );
+
+    final facilityResponse = FacilitiesResponse.fromJson(json.decode(response.body));
+
+    return facilityResponse;
+
+  }  
+
+  Future<FacilitiesResponse> deleteFacility(int id, String token) async {
+    Uri url = Uri.parse('$_baseUrl/deleteFacility/$id');
+
+    final response = await http.delete(
+      url,
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'}
+    );
+
+    final facilityResponse = FacilitiesResponse.fromJson(json.decode(response.body));
+
+    return facilityResponse;
+
+  }
+
+  Future<FacilitiesResponse> activateFacility(int id, String token) async {
+    Uri url = Uri.parse('$_baseUrl/activateFacility/$id');
+
+    final response = await http.post(
+      url,
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'}
+    );
+
+    final facilityResponse = FacilitiesResponse.fromJson(json.decode(response.body));
+
+    return facilityResponse;
+
+  }
+
+  Future<FacilitiesResponse> deactivateFacility(int id, String token) async {
+    Uri url = Uri.parse('$_baseUrl/deactivateFacility/$id');
+
+    final response = await http.post(
+      url,
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'}
+    );
+
+    final facilityResponse = FacilitiesResponse.fromJson(json.decode(response.body));
+
+    return facilityResponse;
+
+  }
+
+  Future<FacilitiesResponse> updateFacility(int id, String name, String openTime, String closeTime, String location, String token) async {
+    Uri url = Uri.parse('$_baseUrl/updateFacility/$id');
+
+    final response = await http.put(
+      url,
+      headers: {'Accept' : 'application/json', 'Authorization' : 'Bearer $token' , 'Content-Type' : 'application/json'}
+    );
+
+    final facilityResponse = FacilitiesResponse.fromJson(json.decode(response.body));
+
+    return facilityResponse;
+    
+  }
+
+}
