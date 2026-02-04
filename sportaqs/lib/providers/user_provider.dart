1d0b0ca56ff334685c3e700467c96e3bbd7d6ffd
Flagn3 <flagn3@gmail.com>
2026-02-04 01:15:32 +0100

    User provider finished

--- sportaqs/lib/providers/user_provider.dart
+++ sportaqs/lib/providers/user_provider.dart
@@ -18,16 +18,16 @@ class UserProvider extends ChangeNotifier {
     loading = true;
     notifyListeners();
 
-    try{
+    try {
       ResponseApi response = await userService.login(username, password);
-      if (response.success && response.data!= null){
+      if (response.success && response.data != null) {
         activeUser = User.fromLoginJson(response.data);
-      } else{
+      } else {
         errorMessage = response.message;
       }
-    } catch (e){
+    } catch (e) {
       errorMessage = e.toString();
-    } finally{
+    } finally {
       loading = false;
       notifyListeners();
     }
@@ -40,4 +40,156 @@ class UserProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+  //register
+  Future<void> register(
+    String name,
+    String secondName,
+    String email,
+    String username,
+    String password,
+  ) async {
+    errorMessage = null;
+    loading = true;
+    notifyListeners();
+
+    try {
+      ResponseApi response = await userService.register(
+        name,
+        secondName,
+        email,
+        username,
+        password,
+      );
+      if (!response.success) {
+        errorMessage = response.message;
+      }
+    } catch (e) {
+      errorMessage = e.toString();
+    } finally {
+      loading = false;
+      notifyListeners();
+    }
+  }
+
+  //Get all Users
+  Future<void> getAllUsers() async {
+    errorMessage = null;
+    loading = true;
+    notifyListeners(); //quitar?
+
+    try {
+      if (activeUser == null || activeUser!.token == null) {
+        errorMessage = 'User not logged or invalid token';
+        loading = false;
+        notifyListeners();
+        return;
+      }
+
+      ResponseApi response = await userService.getAllUsers(activeUser!.token!);
+
+      if (response.success && response.data != null) {
+        userList = (response.data as List)
+            .map((u) => User.fromUserDTO(u))
+            .toList();
+      } else {
+        errorMessage = response.message;
+      }
+    } catch (e) {
+      errorMessage = e.toString();
+    } finally {
+      loading = false;
+      notifyListeners();
+    }
+  }
+
+  //Get user by id?
+
+  //Update User
+  Future<void> updateUser(int id, String name, String secondName) async {
+    errorMessage = null;
+    loading = true;
+    notifyListeners();
+
+    try {
+      if (activeUser == null || activeUser!.token == null) {
+        errorMessage = 'User not logged or invalid token';
+        return;
+      }
+      ResponseApi response = await userService.updateUser(
+        id,
+        name,
+        secondName,
+        activeUser!.token!,
+      );
+
+      if (!response.success) {
+        // await getAllUsers();
+        errorMessage = response.message;
+      }
+    } catch (e) {
+      errorMessage = e.toString();
+    } finally {
+      loading = false;
+      notifyListeners();
+    }
+  }
+
+  // Activate / deactivate
+  Future<void> editActivation(int id, bool isActive) async {
+    errorMessage = null;
+    loading = true;
+    notifyListeners();
+
+    try {
+      ResponseApi response;
+
+      if (activeUser == null || activeUser!.token == null) {
+        errorMessage = 'User not logged or invalid token';
+        return;
+      }
+
+      if (isActive) {
+        response = await userService.deactivateUser(id, activeUser!.token!);
+      } else {
+        response = await userService.activateUser(id, activeUser!.token!);
+      }
+
+      if (response.success) {
+        await getAllUsers();
+      } else {
+        errorMessage = response.message;
+      }
+    } catch (e) {
+      errorMessage = e.toString();
+    } finally {
+      loading = false;
+      notifyListeners();
+    }
+  }
+
+  //Delete
+  Future<void> deleteUser(int id) async {
+    errorMessage = null;
+    loading = true;
+    notifyListeners();
+
+    try {
+      if (activeUser == null || activeUser!.token == null) {
+        errorMessage = 'User not logged or invalid token';
+        return;
+      }
+      ResponseApi response = await userService.deleteUser(
+        id,
+        activeUser!.token!,
+      );
+      if (!response.success) {
+        errorMessage = response.message;
+      }
+    } catch (e) {
+      errorMessage = e.toString();
+    } finally {
+      loading = false;
+      notifyListeners();
+    }
+  }
 }
